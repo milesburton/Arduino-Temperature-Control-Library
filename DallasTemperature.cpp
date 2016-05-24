713852f3d44898d0c1125ce5d86dd296216acb37
Miles Burton <milesburton@users.noreply.github.com>
2016-05-24 07:30:44 +0100

    Update DallasTemperature.cpp

--- DallasTemperature.cpp
+++ DallasTemperature.cpp
@@ -3,10 +3,6 @@
 // License as published by the Free Software Foundation; either
 // version 2.1 of the License, or (at your option) any later version.
 
-// Version 3.7.2 modified on Dec 6, 2011 to support Arduino 1.0
-// See Includes...
-// Modified by Jordan Hochenbaum
-
 #include "DallasTemperature.h"
 
 

df3d4d50fe57fc009611d7426945229e621cc084
James Myatt <james@jamesmyatt.co.uk>
2017-04-11 13:13:26 +0100

    Move internal constants to DallasTemperature.cpp (Closes #71)

--- DallasTemperature.h
+++ DallasTemperature.h
@@ -28,32 +28,6 @@
 #define DS1825MODEL  0x3B
 #define DS28EA00MODEL 0x42
 
-// OneWire commands
-#define STARTCONVO      0x44  // Tells device to take a temperature reading and put it on the scratchpad
-#define COPYSCRATCH     0x48  // Copy EEPROM
-#define READSCRATCH     0xBE  // Read EEPROM
-#define WRITESCRATCH    0x4E  // Write to EEPROM
-#define RECALLSCRATCH   0xB8  // Reload from last known
-#define READPOWERSUPPLY 0xB4  // Determine if device needs parasite power
-#define ALARMSEARCH     0xEC  // Query bus for devices with an alarm condition
-
-// Scratchpad locations
-#define TEMP_LSB        0
-#define TEMP_MSB        1
-#define HIGH_ALARM_TEMP 2
-#define LOW_ALARM_TEMP  3
-#define CONFIGURATION   4
-#define INTERNAL_BYTE   5
-#define COUNT_REMAIN    6
-#define COUNT_PER_C     7
-#define SCRATCHPAD_CRC  8
-
-// Device resolution
-#define TEMP_9_BIT  0x1F //  9 bit
-#define TEMP_10_BIT 0x3F // 10 bit
-#define TEMP_11_BIT 0x5F // 11 bit
-#define TEMP_12_BIT 0x7F // 12 bit
-
 // Error Codes
 #define DEVICE_DISCONNECTED_C -127
 #define DEVICE_DISCONNECTED_F -196.6
@@ -147,10 +121,10 @@ public:
 
     // returns true if the bus requires parasite power
     bool isParasitePowerMode(void);
-    
+
      // Is a conversion complete on the wire?
     bool isConversionComplete(void);
-    
+
     int16_t millisToWaitForConversion(uint8_t);
 
 #if REQUIRESALARMS
